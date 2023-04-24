4e617378b2e28884446d66670f6432c08a721e0a
Tom Smit <tomosapien_66@hotmail.com>
2023-04-24 14:04:47 +0200

    Include a release with an executable to run

--- DefenseSimulation/MissileLauncher.cs
+++ DefenseSimulation/MissileLauncher.cs
@@ -11,6 +11,11 @@ namespace DefenseSimulation
             this.hitProbability = hitProbability;
         }
 
+        /// <summary>
+        /// Simulates the firing of a missile. It checks if the Hit roll is smaller than or equal to the hit probability. 
+        /// If it is, it will return Hit. Otherwise it returns Miss
+        /// </summary>
+        /// <returns>Hit or Miss</returns>
         public FireResult Fire()
         {
             if (SimulateLaunch() <= hitProbability)
@@ -19,6 +24,10 @@ namespace DefenseSimulation
             return FireResult.Miss;
         }
 
+        /// <summary>
+        /// Generate a number between 0.0 and 1.0, uniformly distributed
+        /// </summary>
+        /// <returns>A number between 0.0 and 1.0</returns>
         private static double SimulateLaunch()
         {
             Random random = new Random();
